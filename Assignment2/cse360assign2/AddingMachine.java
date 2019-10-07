5589247b544941182ec4c2ae5791bc5e722e59c6
vanes <vanes@DESKTOP-RFNSIUP.3520.dhcp.asu.edu>
2019-10-07 16:28:16 -0700

    initial commit

---
+++ Assignment2/cse360assign2/AddingMachine.java
@@ -0,0 +1,66 @@
+/** Vanessa Bracken
+ * ASU ID: 1217621743
+ * Assignment 1
+ * Class that implements functions that add and subtract integers
+ * @author vanessa
+ *
+ */
+ package cse360assign2;
+ 
+ /**
+  * Class that declares a @param called total
+  * 
+  */
+
+ public class AddingMachine {
+
+ 	private int total;
+ 	
+ 	public AddingMachine () {
+ 		total = 0;  // not needed - included for clarity
+ 	}
+ 	
+ 	/**
+ 	 * no operations completed
+ 	 * @return 0 
+ 	 */
+ 	
+ 	public int getTotal () {
+ 		return 0;
+ 	}
+ 	
+ 	/**
+ 	 * no operations completed
+ 	 * 
+ 	 * @param value
+ 	 * 
+ 	 */
+ 	
+ 	public void add (int value) {
+ 		
+ 	}
+ 	
+	/**
+ 	 * no operations completed
+ 	 * 
+ 	 * @param value
+ 	 * 
+ 	 */
+ 	
+ 	public void subtract (int value) {
+ 		
+ 	}
+ 	
+ 	/**
+ 	 * @returns empty string
+ 	 *
+ 	 */
+ 		
+ 	public String toString () {
+ 		return "";
+ 	}
+
+ 	public void clear() {
+ 	
+ 	}
+ }
